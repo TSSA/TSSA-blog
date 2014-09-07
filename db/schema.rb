28e4fc0813b6f7897d25186fb0eee98dd6eb7bf6
neofluxs <neofluxs@me.com>
2014-09-07 18:20:57 +0000

    fixed bugs with migration files

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140803234822) do
+ActiveRecord::Schema.define(version: 20140803201934) do
 
   create_table "active_admin_comments", force: true do |t|
     t.string   "namespace"
@@ -56,10 +56,10 @@ ActiveRecord::Schema.define(version: 20140803234822) do
     t.string   "title"
     t.text     "description"
     t.text     "body"
+    t.integer  "author_id"
+    t.integer  "category_id"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "category_id"
-    t.integer  "author_id"
   end
 
 end
